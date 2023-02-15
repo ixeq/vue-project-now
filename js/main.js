00b5ecfe2dbdfe9a0af72a9349460e135a3c3426
ixeq <tim.matyskin@gmail.com>
2023-02-15 23:11:32 +0700

    Practika 6

--- js/main.js
+++ js/main.js
@@ -3,25 +3,25 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks.",
+        selectedVariant: 0,
+        altText: "A pair of socks",
+        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
                 variantId: 2234,
                 variantColor: 'green',
                 variantImage: "./assets/vmSocks-green-onWhite.jpg",
+                variantQuantity: 10,
             },
             {
                 variantId: 2235,
                 variantColor: 'blue',
                 variantImage: "./assets/vmSocks-blue-onWhite.jpg",
+                variantQuantity: 0,
             }
          ],
-        image: "./assets/vmSocks-green-onWhite.jpg",       
-        altText: "A pair of socks",
-        link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
-        inStock: true,
-        inventory: 10,
-        onSale: true,
-        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
     },
@@ -33,10 +33,21 @@ let app = new Vue({
         removeCart() {
             this.cart -= 1
         },
-        updateProduct(variantImage) {
-            this.image = variantImage
+        updateProduct(index) {
+            this.selectedVariant = index;
+        },         
+
+     },
+     computed: {
+        title() {
+            return this.brand + ' ' + this.product;
+        },     
+        image() {
+            return this.variants[this.selectedVariant].variantImage;
         },
-         
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity;
+        },
      }
      
  })
