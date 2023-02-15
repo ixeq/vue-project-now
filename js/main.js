a8f7433a7ae082008605e4cbb464012ff5eabf39
ixeq <tim.matyskin@gmail.com>
2023-02-15 21:47:25 +0700

    Practika 4

--- js/main.js
+++ js/main.js
@@ -5,13 +5,25 @@ let app = new Vue({
     data: {
         product: "Socks",
         description: "A pair of warm, fuzzy socks.",
+        variants: [
+            {
+                variantId: 2234,
+                variantColor: 'green'
+            },
+            {
+                variantId: 2235,
+                variantColor: 'blue'
+            }
+         ],         
         image: "./assets/vmSocks-green-onWhite.jpg",
         image: "./assets/vmSocks-blue-onWhite.jpg",
         altText: "A pair of socks",
         link: "https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks",
         inStock: true,
         inventory: 10,
-        onSale: "On Sale",
+        onSale: true,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
     }
  })
  
